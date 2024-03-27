acdb223cee2db730c9212144a7ad682d43b9e3be
Jake Tyler <3jaketyler@gmail.com>
2024-03-26 23:03:13 -0400

    added more

--- Problem1.py
+++ Problem1.py
@@ -2,6 +2,29 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import multiprocessing as mp
+import random
+
+def generatePoints(boundBox, recList):
+    numInPancake = 0
+    
+    for i in range(10000):
+        x = random.uniform(boundBox[0], boundBox[3])
+        y = random.uniform(boundBox[1], boundBox[4])
+        z = random.uniform(boundBox[2], boundBox[5])
+
+        for rec in recList:
+            xIn = (x >= min(rec[0], rec[3])) and (x <= max(rec[0], rec[3]))
+            yIn = (y >= min(rec[1], rec[4])) and (y <= max(rec[1], rec[4]))
+            zIn = (z >= min(rec[2], rec[5])) and (z <= max(rec[2], rec[5]))
+            if xIn and yIn and zIn:
+                numInPancake += 1
+                break
+
+    return numInPancake
+
+
+
 
 if __name__ == "__main__":
     numRecs = int(input("\nEnter the number of rectangular prisms: "))
@@ -41,5 +64,14 @@ if __name__ == "__main__":
     boundBox.append(max_y)
     boundBox.append(max_z)
 
-    print(recList)
-    print(boundBox)
+    pool = mp.Pool(processes = 150)
+    results = []
+    for i in range(150):
+        result = pool.apply_async(generatePoints, args=(boundBox, recList,))
+        results.append(result)
+    allIn = sum(result.get() for result in results)
+    allGen = 150 * 10000
+    boundVol = (boundBox[3] - boundBox[0]) * (boundBox[4] - boundBox[1]) * (boundBox[5] - boundBox[2])
+    ratio = allIn / allGen
+    volume = ratio * boundVol
+    print("The volume is: " + volume)

c92a662d278994f2f2d5d50849cbafdc37a9656b
Jake Tyler <3jaketyler@gmail.com>
2024-03-28 20:44:15 -0400

    new strat

---
+++ Problem 2/input.py
@@ -0,0 +1,48 @@
+# The program in this file is the individual work of Jake Tyler
+
+import sys
+
+if __name__ == "__main__":
+    numRecs = int(input("\nEnter the number of rectangular prisms: "))
+    degAcc = float(input("\nEnter your desired degree of accuracy: "))
+
+    recList = []
+
+    i = 0
+
+    print("\nEnter x1, y1, z1, x2, y2, and z2 with spaces in between.\n")
+    while i < numRecs:
+        userInput = input()
+        floats = list(map(float, userInput.split()))
+        if len(floats) == 6:
+            recList.append(floats)
+            i += 1
+        else:
+            print("Enter exactly six numbers.")
+
+    boundBox = []
+
+    min_x, min_y, min_z = float('inf'), float('inf'), float('inf')
+    max_x, max_y, max_z = float('-inf'), float('-inf'), float('-inf')
+
+    for rec in recList:
+        min_x = min(min_x, rec[0], rec[3])
+        min_y = min(min_y, rec[1], rec[4])
+        min_z = min(min_z, rec[2], rec[5])
+        max_x = max(max_x, rec[0], rec[3])
+        max_y = max(max_y, rec[1], rec[4])
+        max_z = max(max_z, rec[2], rec[5])
+
+    boundBox.append(min_x)
+    boundBox.append(min_y)
+    boundBox.append(min_z)
+    boundBox.append(max_x)
+    boundBox.append(max_y)
+    boundBox.append(max_z)
+
+    with open("data.txt", "w") as w:
+        w.write(str(numRecs) + "\n")
+        w.write(str(degAcc) + "\n")
+        w.write(" ".join(map(str, boundBox)) + "\n")
+        for rec in recList:
+            w.write(" ".join(map(str, rec)) + "\n")
